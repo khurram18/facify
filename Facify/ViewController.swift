3a53bade131eb444da459b285bda056a2541b9fb
Khurram Shehzad <khurram.pak522@gmail.com>
2018-03-20 19:06:59 +0500

    Initial project

---
+++ Facify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facify
+//
+//  Created by Khurram on 20/03/2018.
+//  Copyright © 2018 Khurram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FacifyTests/FacifyTests.swift
@@ -0,0 +1,36 @@
+//
+//  FacifyTests.swift
+//  FacifyTests
+//
+//  Created by Khurram on 20/03/2018.
+//  Copyright © 2018 Khurram. All rights reserved.
+//
+
+import XCTest
+@testable import Facify
+
+class FacifyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
