504444ca9ef0ecb008d21c080984ebae4140eaa7
Khurram Shehzad <khurram.pak522@gmail.com>
2018-03-28 11:47:28 +0500

    Update acknowledgements screen

--- Facify/ViewControllers/InfoViewController.swift
+++ Facify/ViewControllers/InfoViewController.swift
@@ -9,8 +9,8 @@ import WebKit
 import UIKit
 
 class InfoViewController: UIViewController {
-@IBOutlet weak var webView: WKWebView!
-@IBAction func onDoneTap(_ sender: Any) {
+
+@objc func onDoneTap() {
     presentingViewController?.dismiss(animated: true, completion: nil)
 }
 override func viewDidLoad() {
@@ -18,10 +18,41 @@ override func viewDidLoad() {
     guard let url = Bundle.main.url(forResource: "acknowledgements", withExtension: "html") else {
         return
     }
+    configureToolbar()
+    appDescriptionLabel.text = appDescription
     webView.load(URLRequest(url: url))
 }
 override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
 }
+private func configureToolbar() {
+    let doneItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(onDoneTap))
+    let spacer = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+    let label = UILabel()
+    label.text = "Acknowledgements"
+    label.textAlignment = .center
+    label.textColor = .black
+    let titleItem = UIBarButtonItem(customView: label)
+    let secondSpacer = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+    toolbar.items = [doneItem, spacer, titleItem, secondSpacer]
+}
+private var appDescription: String {
+    guard let infoDictionary = Bundle.main.infoDictionary else {
+        return ""
+    }
+    guard let name = infoDictionary["CFBundleName"] as? String else {
+        return ""
+    }
+    guard let majorVersion = infoDictionary["CFBundleShortVersionString"] as? String else {
+        return name
+    }
+    guard let minorVersion = infoDictionary["CFBundleVersion"] as? String else {
+        return "\(name), Version \(majorVersion)"
+    }
+    return "\(name), Version \(majorVersion) (\(minorVersion))"
+}
+@IBOutlet weak var appDescriptionLabel: UILabel!
+@IBOutlet weak var toolbar: UIToolbar!
+@IBOutlet weak var webView: WKWebView!
 }
